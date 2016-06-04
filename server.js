4ca76f0ee39cb4404b158b8758433467ed91c023
mcdokath <mcdokath@users.noreply.github.com>
2016-06-04 16:55:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-sample
+// Include express
+var express = require('express');
+
+// Create express application
+var app = express();
+
+// Use environment defined port or 3000
+var port = process.env.PORT || 3000;
+
+// Create express router
+var router = express.Router();
+
+// Initial dummy route for testing
+// http://localhost:3000/api
+router.get('/', function(req, res) {
+  res.json({ message: 'You are running dangerously low on beer!' });
+});
+
+// Register all routes with /api
+app.use('/api', router);
+
+// Start the server
+app.listen(port);
+console.log('Insert beer on port ' + port);
