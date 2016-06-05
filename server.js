ff4184a37785ca928a19ac122df6c134f3b79bf9
mcdokath <mcdokath@users.noreply.github.com>
2016-06-04 21:34:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var listController = require('./controllers/list');
 var userController = require('./controllers/user');
 var authController = require('./controllers/auth');
 var clientController = require('./controllers/client');
+var oauth2Controller = require('./controllers/oauth2');
 var ejs = require('ejs');
 var session = require('express-session');
 
@@ -58,6 +59,15 @@ router.route('/users')
 router.route('/clients')
   .post(authController.isAuthenticated, clientController.postClients)
   .get(authController.isAuthenticated, clientController.getClients);
+
+// create endpoint handlers for oauth2 authorize
+router.route('/oauth2/authorize')
+  .get(authController.isAuthenticated, oauth2Controller.authorization)
+  .post(authController.isAuthenticated, oauth2Controller.decision);
+
+// Create endpoint handlers for oauth2 token
+router.route('/oauth2/token')
+  .post(authController.isClientAuthenticated, oauth2Controller.token);
 
 // Register all routes with /api
 app.use('/api', router);
