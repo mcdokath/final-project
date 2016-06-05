2ab7071811eae145ae139cb44a3837c485784f2a
mcdokath <mcdokath@users.noreply.github.com>
2016-06-04 21:16:18 -0700

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,27 +2,20 @@
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 var User = require('../models/user');
+var Client = require('../models/client');
 
-passport.use(new BasicStrategy(
+passport.use('client-basic', new BasicStrategy(
   function(username, password, callback) {
-    User.findOne({ username: username }, function (err, user) {
+    Client.findOne({ id: username }, function (err, client) {
       if (err) { return callback(err); }
       
-      // no user found with that username
-      if (!user) { return callback(null, false); }
+      // no client found with that id or bad password
+      if (!client || client.secret !== password) { return callback(null, false); }
       
-      // verify password
-      user.verifyPassword(password, function(err, isMatch) {
-        if (err) { return callback(err); }
-        
-        // password did not match
-        if (!isMatch) { return callback(null, false); }
-        
-        // password DID match
-        return callback(null, user);
-      });
+      // success
+      return callback(null, client);
     });
   }
 ));
 
-exports.isAuthenticated = passport.authenticate('basic', { session : false });
+exports.isClientAuthenticated = passport.authenticate('client-basic', { session : false });
