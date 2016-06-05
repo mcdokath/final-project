4948e4ebc94186cbe070dc81d67134c10f03b0eb
mcdokath <mcdokath@users.noreply.github.com>
2016-06-04 21:09:17 -0700

    Update client.js

--- models/client.js
+++ models/client.js
@@ -1,1 +1,13 @@
+// load required modules
+var mongoose = require('mongoose');
 
+// define client schema
+var clientSchema = new mongoose.Schema({
+  name: { type: String, unique: true, required: true },
+  id: { type: String, required: true },
+  secret: { type: String, required: true},
+  userId: { type: String, required: true }
+});
+
+// export Mongoose model
+module.exports = mongoose.model('Client', clientSchema);
