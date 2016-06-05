6d61d94e51653b203569da5480561a7508ad2621
mcdokath <mcdokath@users.noreply.github.com>
2016-06-04 20:30:03 -0700

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,1 +1,28 @@
+// load required packages
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+var User = require('../models/user');
 
+passport.use(new BasicStrategy(
+  function(username, password, callback) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return callback(err); }
+      
+      // no user found with that username
+      if (!user) { return callback(null, false); }
+      
+      // verify password
+      user.verifyPassword(password, function(err, isMatch) {
+        if (err) { return callback(err); }
+        
+        // password did not match
+        if (!isMatch) { return callback(null, false); }
+        
+        // password DID match
+        return callback(null, user);
+      });
+    });
+  }
+));
+
+exports.isAuthenticated = passport.authenticate('basic', { session : false });
