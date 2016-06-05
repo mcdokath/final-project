94491e28cf4a28c143d53a2733b7b9278596443a
mcdokath <mcdokath@users.noreply.github.com>
2016-06-04 19:07:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var app = express();
 app.use(bodyParser.urlencoded({
   extended:true
 }));
+app.use(bodyParser.json());
 
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
