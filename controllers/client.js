4e11953c9c8888f24179d9c84eabb4378727af50
mcdokath <mcdokath@users.noreply.github.com>
2016-06-04 21:12:21 -0700

    Update client.js

--- controllers/client.js
+++ controllers/client.js
@@ -1,1 +1,33 @@
+// load required packages
+var Client = require('../models/client');
 
+// create /api/client for POST requests
+exports.postClients = function(req, res) {
+  // create new instance of Client model
+  var client = new Client();
+  
+  // set client properties to POST data
+  client.name = req.body.name;
+  client.id = req.body.id;
+  client.secret = req.body.secret;
+  client.userId = req.user._id;
+  
+  // save client and check for errors
+  client.save(function(err) {
+    if (err)
+      res.send(err);
+      
+    res.json({ message: 'Client added!', data: client });
+  });
+};
+
+// create /api/clients for GET requests
+exports.getClients = function(req, res) {
+  // use Client model to find all clients
+  Client.find({ userId: req.user._id }, function(err, clients) {
+    if (err)
+      res.send(err);
+    
+    res.json(clients);
+  });
+};
