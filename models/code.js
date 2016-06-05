5574da0cbae2f7f2df18f16970fbbe3199b74db4
mcdokath <mcdokath@users.noreply.github.com>
2016-06-04 21:41:50 -0700

    Update code.js

--- models/code.js
+++ models/code.js
@@ -1,13 +1,13 @@
-// load required packages
+// Load required packages
 var mongoose = require('mongoose');
 
-// define token schema
-var CodeSchema = new mongoose.Schema({
+// Define our token schema
+var CodeSchema   = new mongoose.Schema({
   value: { type: String, required: true },
   redirectUri: { type: String, required: true },
   userId: { type: String, required: true },
   clientId: { type: String, required: true }
 });
 
-// export Mongoose model
+// Export the Mongoose model
 module.exports = mongoose.model('Code', CodeSchema);
