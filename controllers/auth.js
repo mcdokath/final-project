62122fba491ad95d22fb5a48dab4a01d4e2dfa79
mcdokath <mcdokath@users.noreply.github.com>
2016-06-04 21:40:13 -0700

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,42 @@
-// load required packages
+// Load required packages
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
+var BearerStrategy = require('passport-http-bearer').Strategy
 var User = require('../models/user');
 var Client = require('../models/client');
-var BearerStrategy = require('passport-http-bearer').Strategy;
 var Token = require('../models/token');
+
+passport.use(new BasicStrategy(
+  function(username, password, callback) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return callback(err); }
+
+      // No user found with that username
+      if (!user) { return callback(null, false); }
+
+      // Make sure the password is correct
+      user.verifyPassword(password, function(err, isMatch) {
+        if (err) { return callback(err); }
+
+        // Password did not match
+        if (!isMatch) { return callback(null, false); }
+
+        // Success
+        return callback(null, user);
+      });
+    });
+  }
+));
 
 passport.use('client-basic', new BasicStrategy(
   function(username, password, callback) {
     Client.findOne({ id: username }, function (err, client) {
       if (err) { return callback(err); }
-      
-      // no client found with that id or bad password
+
+      // No client found with that id or bad password
       if (!client || client.secret !== password) { return callback(null, false); }
-      
-      // success
+
+      // Success
       return callback(null, client);
     });
   }
@@ -42,3 +64,5 @@ passport.use(new BearerStrategy(
 ));
 
 exports.isAuthenticated = passport.authenticate(['basic', 'bearer'], { session : false });
+exports.isClientAuthenticated = passport.authenticate('client-basic', { session : false });
+exports.isBearerAuthenticated = passport.authenticate('bearer', { session: false });
