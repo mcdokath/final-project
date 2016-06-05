2d9a654a7a3551cce47d6d51105e39f82ed2f6bb
mcdokath <mcdokath@users.noreply.github.com>
2016-06-04 20:19:15 -0700

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,2 +1,27 @@
 // load required packages
 var User = require('../models/user');
+
+// create /api/users for POST requests
+exports.postUsers = function(req, res) {
+  var user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  
+  user.save(function(err) {
+    if (err)
+      res.send(err);
+      
+    res.json({ message: 'New user added!' });
+  });
+};
+
+// create /api/users for GET requests
+exports.getUsers = function(req, res) {
+  Users.find(function(err, users) {
+    if (err)
+      res.send(err);
+      
+    res.json(users);
+  });
+};
