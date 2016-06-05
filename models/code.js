8867a4d4397e75c8bd71a65ff521648c33441f3a
mcdokath <mcdokath@users.noreply.github.com>
2016-06-04 21:37:50 -0700

    Update code.js

--- models/code.js
+++ models/code.js
@@ -5,8 +5,8 @@ var mongoose = require('mongoose');
 var CodeSchema = new mongoose.Schema({
   value: { type: String, required: true },
   redirectUri: { type: String, required: true },
-  userId: { type: String, required, true },
-  clientId: { type: String, required, true }
+  userId: { type: String, required: true },
+  clientId: { type: String, required: true }
 });
 
 // export Mongoose model
