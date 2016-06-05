acf0f65ca60ae9ae4717138960ecaae8774f96cd
mcdokath <mcdokath@users.noreply.github.com>
2016-06-04 17:52:56 -0700

    Update list.js

--- models/list.js
+++ models/list.js
@@ -1,1 +1,13 @@
-sample
+// load required packages
+var mongoose = require('mongoose');
+
+// define list schema
+var listSchema = new mongoose.Schema({
+  name: String,
+  item1: String,
+  item2: String,
+  item3: String
+})
+
+// export model
+module.exports = mongoose.model('List', listSchema);
